f3767508e535c483b5918b9fbbef2396ea7989a4
Eric Rojo <ericrojo156@hotmail.com>
2023-09-16 11:57:40 -0700

    minor diffs

--- src/activity/ActivityElement.tsx
+++ src/activity/ActivityElement.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useMemo} from 'react';
+import React, {useMemo} from 'react';
 import {View, Text, StyleSheet, StyleProp, ViewStyle} from 'react-native';
 import * as ColorPalette from '../ColorPalette';
 import CustomPressable from '../components/Pressable';
